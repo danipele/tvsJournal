fc7b00a14290e4d0d27f37757ee5ab7e9b7881e7
Daniel Pele <daniel.pele@unifiedpost.com>
2021-07-08 18:32:59 +0300

    account page

--- app/controllers/devise_auth/registrations_controller.rb
+++ app/controllers/devise_auth/registrations_controller.rb
@@ -13,5 +13,39 @@ module DeviseAuth
         redirect_to new_user_registration_path, alert: resource.errors.full_messages
       end
     end
+
+    def update
+      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+      if resource_updated
+        redirect_to_path
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        redirect_to edit_user_registration_path, alert: resource.errors.full_messages
+      end
+    end
+
+    private
+
+    def without_password?
+      params[:user][:current_password].nil?
+    end
+
+    def resource_updated
+      if without_password?
+        resource.update_without_password(account_update_params)
+      else
+        resource.update_with_password(account_update_params)
+      end
+    end
+
+    def redirect_to_path
+      if without_password?
+        redirect_to edit_user_registration_path, notice: 'account info updated'
+      else
+        redirect_to new_user_session_path, notice: 'password changed. please log in again'
+      end
+    end
   end
 end
